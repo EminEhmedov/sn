5398ffddd29423d182729f45b58d52c9a7c0339f
EminEhmedli <ekoemin91@gmail.com>
2022-05-27 19:08:31 +0400

    first commit

--- script.js
+++ script.js
@@ -1,79 +1,95 @@
 let snake=document.querySelector('.snake')
 
-let snake2=document.querySelector('#border')
-window.addEventListener('keyup',test);
+let snake2=document.querySelector('#border');
+
+window.addEventListener('keydown',test2)
 let leftX=0
 let topX=0
+let k=10
+let y=10
 
 
 
-function test(e){
-    
+function test2(e){
+  
    
     if(e.keyCode==39){
-        setInterval(()=>{
-            leftX++
+        snake.classList.add('active1')
+            snake.classList.remove('active2')
+            snake.classList.remove('active3')
+            snake.classList.remove('active4')
+            leftX+=y
             snake.style.left=`${leftX}px`
-            // console.log(leftX);
-            if(leftX>550){
+          
+            if(leftX>540){
              leftX=0
             }
            
                
-        },1)
-
+       
    
   
     }
 
-
-
-    else if(e.keyCode==37){
-  
-        let set2=setInterval(()=>{
-            leftX--
-            snake.style.left=`${leftX}px`
-            if(leftX<0){
-                leftX=550
-               }
-        },1)
-       
+    if(e.keyCode==37){
       
-            }
+        snake.classList.remove('active1')
+        snake.classList.add('active2')
+        snake.classList.remove('active3')
+        snake.classList.remove('active4')
+        leftX-=y
+        snake.style.left=`${leftX}px`
+        
+        if(leftX<0){
+            leftX=540
+           }
+
+   
+  
+        }
 
 
 
-            if(e.keyCode==40){
-              
-                let set3=  setInterval(()=>{
-                    topX++
-                    snake.style.top=`${topX}px`
-                    if(topX>550){
-                        topX=0
-                       }
-                },1)
-                    }
+        if(e.keyCode==40){
+            snake.classList.remove('active2')
+            snake.classList.remove('active1')
+            snake.classList.remove('active3')
+            snake.classList.add('active4')
+                topX+=k
+                snake.style.top=`${topX}px`
+                if(topX>540){
+                    topX=0
+                   }
+          
+                }
 
 
 
-                    if(e.keyCode==38){
-                       
-                        let set4=  setInterval(()=>{
-                            topX--
-                            snake.style.top=`${topX}px`
-                            if(topX<0){
-                                topX=550
-                               }
-                        },1)
-                            }
+                if(e.keyCode==38){
+                    snake.classList.remove('active2')
+                    snake.classList.remove('active1')
+                    snake.classList.add('active3')
+                    snake.classList.remove('active4')
+                    
+                  
+                        topX-=k
+                        snake.style.top=`${topX}px`
+                        if(topX<0){
+                            topX=540
+                           }
+                   
+                        }
+
+   
 }
 
-setInterval(()=>{
+setInterval(test,10000)
+
+
+function test(){
     let random=Math.floor(Math.random()*540)
     let random1=Math.floor(Math.random()*500)
-    // console.log(random1);
-    // console.log(random);
-    // console.log(random1);
+    
     const para = document.createElement("div");
    
    para.classList.add('active')
@@ -86,21 +102,23 @@ let divsay=document.querySelectorAll('.active')
 
 if(divsay.length==2){
     const divsay2=document.querySelector('.active:nth-child(1)')
-//  console.log('salam');
+
  divsay2.remove();
- 
-    //     console.log(leftX);
-    // console.log(topX);   
 }
-setInterval(()=>{
-    
-    // console.log(leftX);
-    if( random==topX-50 && random1==leftX-50){
-        console.log('berbaredir');
-       
-    }
-},10)
-},10000)
+
+setInterval(yoxla,0.5)
+
+function yoxla(){
+  
+
+  
+
+}
+
+
+
+}
+
 
 
 
